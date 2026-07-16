d0446a28645529f86a7186dcc0cf2dcb47944884
Estuardo Vasquez <estuardovasquezaz@gmail.com>
2026-07-16 16:04:22 -0600

    Agrega sitemap automático y robots.txt para SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import remarkBreaks from 'remark-breaks';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://azinmobiliarios-web.pages.dev',
+  integrations: [sitemap()],
   markdown: {
     remarkPlugins: [remarkBreaks],
   },
